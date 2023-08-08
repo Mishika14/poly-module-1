d7509ecf1d20d2aadad513e580c37d8c32bb6326
Mishika14 <95122524+Mishika14@users.noreply.github.com>
2023-08-08 20:58:53 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -5,13 +5,13 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+const tokenContractJSON = require("../artifacts/contracts/friends.sol/friends.json");
 
-const tokenAddress = ""; // place your erc20 contract address here
+const tokenAddress = "0x6905428DEB23FeFB512cC6C5bD7160F0B4AB1872"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const walletAddress = "0x0Bd439D494015ff3B46e6f027E1fbd5a9Fc67Be3"; // place your public address for your wallet here
 
-async function main() {
+async function main() { 
 
     const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
 
